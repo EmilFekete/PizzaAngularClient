3f85dda87865e12555de53007d348a72f435dd93
Fekete Emil <fekete.feketeemil>
2017-12-02 13:07:35 +0100

    Add Firestore dependency, Temp code to test Firestore connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { FormsModule, NgModel } from '@angular/forms';
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface PostId extends Post {
+  id: string;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: any;
+  title: string;
+  content: string;
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  constructor(private firestore: AngularFirestore) {
+
+  }
+
+  public ngOnInit(): void {
+    this.postsCol = this.firestore.collection('posts');
+    this.posts = this.postsCol.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return { id, data };
+        });
+      });
+
+  }
+
+  addPost() {
+    this.firestore.collection('posts').add({ 'title': this.title, 'content': this.content });
+  }
+
+  getPost(postId) {
+    this.postDoc = this.firestore.doc('posts/' + postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(postId) {
+    this.firestore.doc('posts/' + postId).delete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCcUPLOgDMG20ng1dMBFmZPruLn5YVEBzQ",
+      authDomain: "pizzawebapp3000.firebaseapp.com",
+      databaseURL: "https://pizzawebapp3000.firebaseio.com",
+      projectId: "pizzawebapp3000",
+      storageBucket: "pizzawebapp3000.appspot.com",
+      messagingSenderId: "221845672726"
+    }),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
@@ -18,4 +30,4 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 export class AppModule {
   public isCollapsed = false;
 
- }
+}
